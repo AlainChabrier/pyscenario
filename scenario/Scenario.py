cc8e9c6fced678604856cc3786276dd2cd33fbf9
Alain Chabrier <achabrier@es.ibm.com>
2017-03-06 10:39:41 +0100

    pyscenario

--- scenario/Scenario.py
+++ scenario/Scenario.py
@@ -13,6 +13,21 @@ class Scenario:
     def __str__(self):
         return self.name
 
+    def duplicate(self, name):
+        newsc = Scenario(name)
+        for table in self.getTables():
+            newsc.put(table, self.getAsDataFrame(table))
+        return newsc
+
+    def getTables(self):
+        tables = []
+        for document in self.db:
+            if document._document_id.startswith(self.name):
+                a = len(self.name)+1
+                b = len(document._document_id)
+                tables.append(document._document_id[a: b])
+        return tables
+
     @staticmethod
     def getInvokingScenario():
         return Scenario("Scenario1")
@@ -22,15 +37,16 @@ class Scenario:
         data = self.db[id]
         return pd.read_json(data['value'])
 
-    def put(self, table, pd):
-        id = self.name+"_"+table
-        if (Document(self.db, id).exists()):
-            data = self.db[id]
-            data['value'] = pd.to_json()
-            data.save()
-        else:
-            data = {
-            '_id': id,
-            'value': pd.to_json()
-            }
-            self.db.create_document(data)
+    def put(self, table, what):
+        if isinstance(what, pd.DataFrame):
+            id = self.name+"_"+table
+            if (Document(self.db, id).exists()):
+                data = self.db[id]
+                data['value'] = what.to_json()
+                data.save()
+            else:
+                data = {
+                '_id': id,
+                'value': what.to_json()
+                }
+                self.db.create_document(data)
